aa5473a2a5a4a3be42738db5a13dd9f460b763e4
hoon <hoonnotes@naver.com>
2022-07-08 11:45:37 +0900

    refactor: changed tweet service to retrieve tweets data from server using fetch api

--- src/service/tweet.js
+++ src/service/tweet.js
@@ -1,43 +1,49 @@
 export default class TweetService {
-  tweets = [
-    {
-      id: 1,
-      text: '드림코딩에서 강의 들으면 너무 좋으다',
-      createdAt: '2021-05-09T04:20:57.000Z',
-      name: 'Bob',
-      username: 'bob',
-      url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-    },
-  ];
+	async getTweets(username) {
+		const res = await fetch(`${process.env.REACT_APP_BASE_URL}/tweets`);
+		const data = await res.json();
 
-  async getTweets(username) {
-    return username
-      ? this.tweets.filter((tweet) => tweet.username === username)
-      : this.tweets;
-  }
+		return data;
+	}
 
-  async postTweet(text) {
-    const tweet = {
-      id: Date.now(),
-      createdAt: new Date(),
-      name: 'Ellie',
-      username: 'ellie',
-      text,
-    };
-    this.tweets.push(tweet);
-    return tweet;
-  }
+	async postTweet(text) {
+		const tweet = {
+			id: Date.now(),
+			createdAt: new Date(),
+			name: "Ellie",
+			username: "ellie",
+			text,
+		};
 
-  async deleteTweet(tweetId) {
-    this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId);
-  }
+		const res = await fetch(`${process.env.REACT_APP_BASE_URL}/tweets`, {
+			method: "POST",
+			body: JSON.stringify(tweet),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
 
-  async updateTweet(tweetId, text) {
-    const tweet = this.tweets.find((tweet) => tweet.id === tweetId);
-    if (!tweet) {
-      throw new Error('tweet not found!');
-    }
-    tweet.text = text;
-    return tweet;
-  }
+		const data = await res.json();
+
+		return data;
+	}
+
+	async deleteTweet(tweetId) {
+		return fetch(`${process.env.REACT_APP_BASE_URL}/tweets/${tweetId}`, {
+			method: "DELETE",
+		});
+	}
+
+	async updateTweet(tweetId, text) {
+		const res = await fetch(`${process.env.REACT_APP_BASE_URL}/tweets/${tweetId}`, {
+			method: "PUT",
+			body: JSON.stringify({ text }),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const data = await res.json();
+
+		return data;
+	}
 }
